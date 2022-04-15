88964ef13bf78f1b68f1c7b07ea0e65fdb7ea555
Michael Chung <mikechung89@gmail.com>
2022-04-15 14:36:54 -0400

    commit tree 9ae55014f2e35c80419f7a97b087ff6798abe53a

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
-  
+  get '/messages' do
+    Message.all.order(created_at: :asc).to_json
+  end
+
+  post '/messages' do
+    message = Message.create(body: params[:body], username: params[:username], created_at: Date.new, updated_at: Date.new)
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    patched_message = Message.find(params[:id])
+    patched_message.update(body: params[:body], updated_at: Date.new)
+    patched_message.to_json
+  end
+
+  delete '/messages/:id' do
+    Message.find(params[:id]).destroy.to_json
+  end
 end
